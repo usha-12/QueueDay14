f787aa8222a1f8016515d65c4422a77d61af0262
usha-12 <usha12ahirwar@gmail.com>
2022-09-15 21:47:14 +0530

    Ability to create a Queue of 56->30->70

--- src/Queue/programs/Node.java
+++ src/Queue/programs/Node.java
@@ -24,3 +24,11 @@ public class Node<T> {
     }
 
 }
+/*Ability to create a Queue of
+56->30->70
+- Use LinkedList to do the Queue
+Operations
+- Here enqueu will internally call append
+method on LinkedList.
+- So 56 will be added first then 30 and
+then 70 to make 56 on top of the Stack*/
