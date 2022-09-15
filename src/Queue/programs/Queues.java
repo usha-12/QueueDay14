0c4946fa2c282b8d8a232a7564bd694e0d454e7f
usha-12 <usha12ahirwar@gmail.com>
2022-09-15 21:47:29 +0530

    Ability to create a Queue of 56->30->70

--- src/Queue/programs/Queues.java
+++ src/Queue/programs/Queues.java
@@ -50,3 +50,11 @@ public class Queues <T extends Comparable<T>>{
         return (list.getSize());
     }
 }
+/*Ability to create a Queue of
+56->30->70
+- Use LinkedList to do the Queue
+Operations
+- Here enqueu will internally call append
+method on LinkedList.
+- So 56 will be added first then 30 and
+then 70 to make 56 on top of the Stack*/
