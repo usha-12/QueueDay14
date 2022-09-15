810d8330cdfadf29b6d952e0731170b25307dfb1
usha-12 <usha12ahirwar@gmail.com>
2022-09-15 21:52:23 +0530

    Ability to dequeue from the beginning

--- src/Queue/programs/Queues.java
+++ src/Queue/programs/Queues.java
@@ -50,11 +50,8 @@ public class Queues <T extends Comparable<T>>{
         return (list.getSize());
     }
 }
-/*Ability to create a Queue of
-56->30->70
-- Use LinkedList to do the Queue
-Operations
-- Here enqueu will internally call append
-method on LinkedList.
-- So 56 will be added first then 30 and
-then 70 to make 56 on top of the Stack*/
+/*Ability to dequeue
+from the beginning
+
+-Use LinkedList to do the
+Queue Operations*/
